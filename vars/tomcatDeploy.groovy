f7eeeb4ff52009ea98d4db6aa6ada24489e2b9b6
javahometech <hari.kammana@gmail.com>
2023-08-31 09:32:24 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,3 +1,10 @@
-def call(){
-  echo "deploying to tomcat"
+def call(tomcatIp, tomcatCreds){
+   sshagent([tomcatCreds]) {
+      // copy war file to tomcat
+      sh "scp -o StrictHostKeyChecking=no target/online-banking.war ec2-user@${tomcatIp}:/opt/tomcat9/webapps"
+
+      // Stop & Start Tomcat
+      sh "ssh ec2-user@${tomcatIp} /opt/tomcat9/bin/shutdown.sh"
+      sh "ssh ec2-user@${tomcatIp} /opt/tomcat9/bin/startup.sh"
+  }
 }
